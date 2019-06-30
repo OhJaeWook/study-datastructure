a2023d1c902899e3d66b242bb2a64f882d935097
jewook89 <jewook89@gmail.com>
2019-06-30 23:45:12 +0900

    우선순위 리스트 다시 해보기

--- workspace-study/6월_22일 최저가 탐색 우선순위 삽입정렬.cpp
+++ workspace-study/6월_22일 최저가 탐색 우선순위 삽입정렬.cpp
@@ -55,52 +55,65 @@ void Init(int Map_size) {
 	}
 }
 
+bool isPriorityHigh(int target, int source) {
+
+	if (a[target].price > a[source].price) {
+		return true;
+	}
+	else if ((a[target].price == a[source].price)
+		&& (a[target].rating < a[source].rating)) {
+		return true;
+	}
+	else if ((a[target].price == a[source].price)
+		&& (a[target].rating == a[source].rating)
+		&& (target < source)) {
+		return true;
+	}
+
+	return false;
+}
+
+
+
 void insertAsPriority(int bid, int y, int x) {
 
-
+	// 처음이면 HEAD에 연결하고 RETURN
 	if (map[y][x] == 0) {
 		map[y][x] = bid;
 		return;
 	}
 
+	// 아니면 prev를 저장하고 prev와 연결해준다
 	int start = map[y][x];
 	int prev = start;
+	int pprev;
 
 
 	while(1){
 
-		if (a[prev].price > a[bid].price) {
+		if (isPriorityHigh(prev, bid)) {
 			break;
 		}
-		else if((a[prev].price == a[bid].price)
-			&&(a[prev].rating < a[bid].rating) ){
-			break;
-		}
-		else if ((a[prev].price == a[bid].price)
-			&& (a[prev].rating == a[bid].rating)
-			&& (prev < bid)) {
-			break;
-		}
-
-
+		
 		if (a[prev].next == 0) {
 			a[prev].next = bid;
 			return;
 		}
-		
+		pprev = prev;
 		prev = a[prev].next;
 	}
 
-	a[bid].next = prev;
-	
 	if (prev == start) {
 		map[y][x] = bid;
 	}
+	else {
+		a[pprev].next = bid;
+	}
+
+	a[bid].next = prev;
 
 	return;
 }
-
-
 
 void AddHouse(int bid, int price, int rating, int y, int x) {
 
@@ -117,6 +130,7 @@ void testPrint(int y, int x) {
 	while (c != 0) {
 		printf("[bid :%d , price : %d , rating %d ] -> ",c, a[c].price, a[c].rating);
 		c = a[c].next;
+		printf("\n");
 	}
 	printf("END");
 
@@ -125,12 +139,35 @@ void testPrint(int y, int x) {
 
 int reserve(int bid, int cday, int night) {
 
+	for (int i = cday; i < cday + night; i++) {
+		if (a[bid].sch[i] != 0) {
+			return 0;
+		}
+	}
+
+	a[bid].sch[cday] = 1;
+
+	for (int i = cday + 1 ; i <= cday + night; i++) {
+		a[bid].sch[i] = 2;
+	}
+
 	return 1;
 }
 
 int cancel(int bid, int cday) {
 
-	return 1;
+	if (a[bid].sch[cday] == 1) {
+
+		int i = cday;
+
+		while(a[bid].sch[i] == 2){
+			a[bid].sch[cday] = 0;
+			i++;
+		}
+		return 1;
+	}
+
+	return 0;
 }
 
 int seach(int opt, int cday, int night, int y, int x, int top[5]) {
@@ -139,12 +176,22 @@ int seach(int opt, int cday, int night, int y, int x, int top[5]) {
 }
 
 int main() {
-	AddHouse(10, 100, 3, 1, 1);
-	AddHouse(11, 300, 3, 1, 1);
-	AddHouse(12, 400, 3, 1, 1);
-	AddHouse(13, 100, 5, 1, 1);
+	AddHouse(1, 100, 3, 1, 1);
+	AddHouse(2, 300, 3, 1, 1);
+	AddHouse(3, 400, 3, 1, 1);
+	AddHouse(4, 100, 5, 1, 1);
+	AddHouse(5, 100, 4, 1, 1);
 
 	testPrint(1, 1);
+	
+	printf("%d \n", reserve(2, 2, 3));
+	printf("%d \n", reserve(2, 3, 4));
+	printf("%d \n", reserve(2, 6, 1));
+
+	printf("%d \n", cancel(2, 2));
+	printf("%d \n", cancel(2, 5));
+	printf("%d \n", cancel(2, 6));
+
 
 }
 
